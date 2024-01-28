2e807f038d9cb3e01fceb293c41c24e97800dc03
Falco Girgis <gyrovorbis@gmail.com>
2024-01-27 22:16:42 -0600

    MAYBE THIS TIME!?

--- gl-pvr.c
+++ gl-pvr.c
@@ -96,12 +96,8 @@ static inline void pvr_hdr_submit(const GLuint *src) {
 }
 #else
 
-#define pvr_list_submit(src, n) do { \
-        pvr_sq_load(NULL, (src), n << 5, PVR_DMA_TA); \
-        sq_wait(); \
-    } while(0)
-
-#define pvr_hdr_submit(src) pvr_sq_load(NULL, (src), 32, PVR_DMA_TA)
+#define pvr_list_submit(src, n) sq_cpy(PVR_TA_INPUT, (src), ((n) << 5))
+#define pvr_hdr_submit(src) sq_cpy(PVR_TA_INPUT, (src), 32)
 
 #endif
 
